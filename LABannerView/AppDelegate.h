c3536149b3bd858b58baae675da440831e042684
libertyAir <libertyair@126.com>
2017-07-11 10:37:39 +0800

    Initial Commit

---
+++ LABannerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LABannerView
+//
+//  Created by libertyAir on 2017/7/11.
+//  Copyright © 2017年 libertyAir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
